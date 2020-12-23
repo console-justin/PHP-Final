8826e3220f812c91df274b8e3d2d1e64a63d6cc3
console-justin <58694791+console-justin@users.noreply.github.com>
2020-12-23 15:23:19 -0500

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'final_project';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+	
+?>
